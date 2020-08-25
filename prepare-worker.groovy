5bd8619ef8988646024b595bbd05efa0a5c6212c
ksalrin <salrindesigngeek@gmail.com>
2020-08-25 23:58:25 +0000

    updated worker

--- prepare-worker.groovy
+++ prepare-worker.groovy
@@ -1,7 +1,7 @@
 node ('master') {
     stage('Init'){
         withCredentials([sshUserPrivateKey(credentialsId: 'root', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-            sh 'ssh -i $SSHKEY root@104.131.103.232 yum install epel-release -y'
+            sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY root@104.131.103.232 yum install epel-release -y'
         }  
     }
 }
