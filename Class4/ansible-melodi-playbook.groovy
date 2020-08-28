919e0bf439f49a77cc9d37d5b85a16e5cc7118d1
ksalrin <salrindesigngeek@gmail.com>
2020-08-28 01:03:00 +0000

    updated

--- Class4/ansible-melodi-playbook.groovy
+++ Class4/ansible-melodi-playbook.groovy
@@ -6,13 +6,13 @@ properties([
     ])
 if (nodeIP?.trim()) {
     node {
-           withCredentials([sshUserPrivateKey(credentialsId: 'root', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-                stage('Pull Repo') {
-                    git branch: '${branch}', changelog: false, poll: false, url: 'https://github.com/ikambarov/ansible-melodi.git'
-                }
-                withEnv(['ANSIBLE_HOST_KEY_CHECKING=False']) {
+         withCredentials([sshUserPrivateKey(credentialsId: 'root', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+            stage('Pull Repo') {
+                git branch: 'master', changelog: false, poll: false, url: 'https://github.com/ikambarov/ansible-melodi.git'
+            }
+            withEnv(['ANSIBLE_HOST_KEY_CHECKING=False']) {
                 stage("Install Apache"){
-                    sh 'ansible-playbook -i "104.131.113.213," --private-key $SSHKEY main.yml'
+                    sh 'ansible-playbook -i "${nodeIP}," -e "melodi_branch=${branch}" --private-key $SSHKEY main.yml'
                     }
                 }  
             }  
