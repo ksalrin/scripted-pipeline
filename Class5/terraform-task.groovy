404a51fca7e66f7c29d5e71536682e84c831cf86
ksalrin <salrindesigngeek@gmail.com>
2020-08-29 22:19:27 +0000

    class5

--- Class5/terraform-task.groovy
+++ Class5/terraform-task.groovy
@@ -2,13 +2,21 @@ node {
     stage("Pull Repo"){ 
          git branch: 'solution', changelog: false, poll: false, url: 'https://github.com/ikambarov/terraform-task.git'
     }
-    stage("Terraform Init"){ 
-        ssh '''
-        cd sandbox/
-        terraform init
-        '''
+    withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+        stage("Terraform Init"){ 
+          sh '''
+            terraform-0.13 version
+            cd sandbox/
+            terraform-0.13 init
+          '''
+        }
     }
-    stage("Terraform Apply"){ 
-
+    withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+      stage("Terraform Apply"){ 
+        sh '''
+           cd sandbox/
+           terraform-0.13 apply -auto-approve
+           '''
+        }
     }
 }
