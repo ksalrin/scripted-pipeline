00efcdb130dcd06bb975c41033678ecdb871d168
ksalrin <salrindesigngeek@gmail.com>
2020-08-29 19:41:21 +0000

    terraform updated

---
+++ Class5/terraform-task.groovy
@@ -0,0 +1,14 @@
+node {
+    stage("Pull Repo"){ 
+         git branch: 'solution', changelog: false, poll: false, url: 'https://github.com/ikambarov/terraform-task.git'
+    }
+    stage("Terraform Init"){ 
+        ssh '''
+        cd sandbox/
+        terraform init
+        '''
+    }
+    stage("Terraform Apply"){ 
+
+    }
+}
